24097ea0246864796775f6fb140f8d3f09850759
RaymondElisha <94136390+RaymondElisha@users.noreply.github.com>
2024-04-09 13:01:34 +0300

    Update settings.py

--- Alegacy/settings.py
+++ Alegacy/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
-
+import dj_database_url
 import os
 from pathlib import Path
 
@@ -87,15 +87,13 @@ WSGI_APPLICATION = 'Alegacy.wsgi.application'
 #        'NAME': BASE_DIR / 'db.sqlite3',
 #    }
 #}
+# Replace the SQLite DATABASES configuration with PostgreSQL:
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        'USER': 'postgres',
-        'PASSWORD': '1234',
-        'HOST':'',
-        'PORT': '',
-    }
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        conn_max_age=600
+    )
 }
 
 # Password validation
